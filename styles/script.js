346b024478400fe432c69bb310772faf70dd6db1
Sammiejohnsonnl <147673641+Sammiejohnsonnl@users.noreply.github.com>
2024-01-03 23:00:35 +0100

    Add files via upload

---
+++ styles/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleBtn = document.getElementById('toggleBtn');
+    const menu = document.querySelector('.menu');
+  
+    toggleBtn.addEventListener('click', function () {
+      menu.classList.toggle('show');
+    });
+  });
+  
